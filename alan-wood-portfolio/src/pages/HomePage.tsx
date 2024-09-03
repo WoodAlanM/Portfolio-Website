c8e17729da194f859e3f6174890783c861e4717f
Alan Wood <WoodAlanM@gmail.com>
2024-09-03 02:25:40 -0400

    Added animations and highlighted resume areas.

--- alan-wood-portfolio/src/pages/HomePage.tsx
+++ alan-wood-portfolio/src/pages/HomePage.tsx
@@ -1,9 +1,34 @@
+import React, {useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
+import resume from '../assets/homepage/resume.png'
+import "../styles/homepage.css"
 
 function HomePage() {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
     <>
-      <h1>Beans</h1>
+      <div className="container-fluid">
+        <div className='d-block mx-auto text-center image-container'>
+          <img 
+            src={resume} 
+            alt="Alan Wood Resume"
+            className={`full-height-image ${isVisible ? 'fade-in' : ''}`}
+          />
+          <div className='highlight highlight-profile'></div>
+          <div className='highlight highlight-work'></div>
+          <div className='highlight highlight-certifications'></div>
+          <div className='highlight highlight-contact'></div>
+          <div className='highlight highlight-skills'></div>
+          <div className='highlight highlight-education'></div>
+          <div className='highlight highlight-languages'></div>
+        </div>
+      </div>
     </>
   );
 }
